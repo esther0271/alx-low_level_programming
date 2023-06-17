d58a02977233438c9987f735caaecd87c0e3017d
esther sithole <sitholester0@gmail.com>
2023-06-17 10:31:06 -0700

    puts to print

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.>
 /**
- * main - Entry point
+ * main - it prints out output using puts
  *
- * Return: Always 0 (Success)
+ * Return: always (0)
+ *
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
