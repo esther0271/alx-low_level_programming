1fc82ea9566b170ead1121fca9c88634cbf7e207
esther sithole <sitholester0@gmail.com>
2023-06-17 11:53:10 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,1 +1,12 @@
+#include <stdio.h>
+/**
+ * main - it prints out output using printf
+ *
+ * Return: always (0)
+ *
+ */
+int main(void)
+{
+	printf("\"with proper grammar, but the outcome is a piece of art,");
+	return (0);
 }
