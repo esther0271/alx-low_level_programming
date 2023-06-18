288a3b331107a97421cda48f1281a06293c6d60b
esther sithole <sitholester0@gmail.com>
2023-06-18 15:22:29 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,18 +6,17 @@
  */
 int main(void)
 {
-	int n;
+	int i = '0';
 
-	for (n = 48; n < 58; n++)
+	while (i <= '9')
 	{
-	putchar(n);
-	if (n != 57)
+	putchar(i);
+	if (i != '9')
 	{
 	putchar(',');
-
-	putchar(' ');
 	putchar(' ');
 	}
+	++i;
 	}
 	putchar('\n');
 	return (0);
