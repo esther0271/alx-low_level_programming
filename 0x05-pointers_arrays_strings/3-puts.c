5fb5dcefe799540c7545e4acf18b767fbb266ee0
esther sithole <sitholester0@gmail.com>
2023-06-26 14:26:24 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts - prints a strin
+ * @str: pointer to th string
+ * Return: string and a new line
+ *
+ */
+
+void _puts(char *str)
+{
+	while (*str[i])
+	{
+	_putchar(str[i]);
+	i++;
+	}
+	_putchar('\n');
+}
