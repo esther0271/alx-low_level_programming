9f76f93aa05fa24c248c19d11c89e104083abbec
esther sithole <sitholester0@gmail.com>
2023-07-13 15:18:37 -0700

    C - More malloc, free

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * ptint_numbers - print rages 0 - 9
+ *
+ * Return: no return
+ *
+ */
+void print_numbers(void)
+{
+	int pr;
+
+	for (pr = 48; pr <= 57; pr++)
+	{
+	_putchar(pr);
+	}
+	_putchar('\n');
+}
